cc2c85e9a022035d7e9e6775067d35f9ff85cbd5
Njteh <njtehkk96@gmail.com>
2020-01-27 18:40:36 +0400

    Keypress Events

---
+++ JS/keydownEvents.js
@@ -0,0 +1,132 @@
+document.addEventListener('keypress', function (event) {
+    console.log(event.which);
+    console.log(event.key);
+    let buttons = {
+        number: null,
+        operation: null,
+        power: null,
+        trig: null,
+        delete: null,
+        equals: false
+    };
+    switch (event.which) {
+
+        case 43:
+            buttons.operation = event.key;
+            break;
+        case 45:
+            buttons.operation = event.key;
+            break;
+        case 42:
+            buttons.operation = 'x';
+            break;
+        case 47:
+            buttons.operation = '÷';
+            break;
+        case 48:
+            buttons.number = event.key;
+            break;
+        case 49:
+            buttons.number = event.key;
+            break;
+        case 50:
+            buttons.number = event.key;
+            break;
+        case 51:
+            buttons.number = event.key;
+            break;
+        case 52:
+            buttons.number = event.key;
+            break;
+        case 53:
+            buttons.number = event.key;
+            break;
+        case 54:
+            buttons.number = event.key;
+            break;
+        case 55:
+            buttons.number = event.key;
+            break;
+        case 56:
+            buttons.number = event.key;
+            break;
+        case 57:
+            buttons.number = event.key;
+            break;
+        case 115:
+            buttons.trig = 'Sin';
+            break;
+        case 83:
+            buttons.trig = 'ArcSin';
+            break;
+        case 116:
+            buttons.trig = 'Tan';
+            break;
+        case 84:
+            buttons.trig = 'ArcTan';
+            break;
+        case 99:
+            buttons.trig = 'Cos';
+            break;
+        case 67:
+            buttons.trig = 'ArcCos';
+            break;
+        case 113:
+            buttons.power = '√';
+            break;
+        case 113:
+            buttons.power = '√';
+            break;
+        case 112:
+            buttons.power = 'x<sup>2</sup>';
+            break;
+        case 80:
+            buttons.power = 'x<sup>n</sup>';
+            break;
+        case 61:
+            buttons.equals = true;
+            break;
+        case 13:
+            buttons.equals = true;
+            break;
+        case 100:
+            buttons.delete = "Delete";
+            break;
+        case 68:
+            buttons.delete = "CE";
+            break;
+            
+        default:
+            break;
+    }
+
+if(buttons.number) {
+    calculator.appendNumber(buttons.number);
+    buttons.number = null;
+}
+if(buttons.operation){
+    calculator.selectOperation(buttons.operation);
+    buttons.operation = null;
+}
+if(buttons.trig){
+    calculator.selectTrigOperation(buttons.trig);
+    buttons.trig = null;
+}
+if(buttons.power){
+    calculator.selectPower(buttons.power);
+    buttons.power = null;
+}
+if(buttons.equals){
+    calculator.compute();
+    buttons.equals = false;
+}
+if(buttons.delete === "Delete"){
+calculator.delete();
+buttons.delete = null;
+}else if(buttons.delete==="CE"){
+calculator.clearAll();
+buttons.delete = null;
+}
+calculator.updateDisplay();
+})
+
--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,39 @@
-function Calculator (displayExpression, displayAnswer){
-    this.displayExpression= displayExpression;
-    this.displayAnswer= displayAnswer;
+function Calculator(displayExpression, displayAnswer) {
+    this.displayExpression = displayExpression;
+    this.displayAnswer = displayAnswer;
 
-    this.expression='';  
-    this.answer='';
+    this.expression = '';
+    this.answer = '';
     this.operation = null;
-    this.trigOperation =null;
+    this.trigOperation = null;
     this.power = null;
 
 }
- 
+
 Calculator.prototype.clearAll = function () {
-        this.expression='';
-        this.answer='';
-        this.operation = null;
-        this.trigOperation = null;
-        this.power=null;
-    
+    this.expression = '';
+    this.answer = '';
+    this.operation = null;
+    this.trigOperation = null;
+    this.power = null;
+
 }
 
-Calculator.prototype.delete = function() {
+Calculator.prototype.delete = function () {
     this.answer = this.answer.slice(0, -1);
 }
 
 
-Calculator.prototype.appendNumber = function(number) {
-    if ( number === '.' &&  this.answer.includes('.')) return;
+Calculator.prototype.appendNumber = function (number) {
+    if (number === '.' && this.answer.includes('.')) return;
     this.answer = this.answer.toString() + number.toString();
-    
+
 }
 
-Calculator.prototype.selectOperation = function (operation){
-    if(this.answer === '' ) return;
-    
-    if(this.expression !== '') {
+Calculator.prototype.selectOperation = function (operation) {
+    if (this.answer === '') return;
+
+    if (this.expression !== '') {
         this.compute();
     }
     this.operation = operation;
@@ -42,48 +42,48 @@ Calculator.prototype.selectOperation = function (operation){
 }
 
 
-Calculator.prototype.selectPower = function(operation){
-    if(this.answer==='') return;
-    if(this.trigOperation)return;
-    if(!this.expression){
+Calculator.prototype.selectPower = function (operation) {
+    if (this.answer === '') return;
+    if (this.trigOperation) return;
+    if (!this.expression) {
 
-        if(operation.toString()==='√' || operation.toString()==='x<sup>2</sup>'){
+        if (operation.toString() === '√' || operation.toString() === 'x<sup>2</sup>') {
             console.log('in here');
             this.power = operation;
             this.compute();
             return;
         }
-    }else{      
-    this.compute();
+    } else {
+        this.compute();
     }
-    
-   this.power = operation;
-   this.expression = this.answer;
-   this.answer= '';
+
+    this.power = operation;
+    this.expression = this.answer;
+    this.answer = '';
 }
 
-Calculator.prototype.selectTrigOperation = function (trigOperation){
+Calculator.prototype.selectTrigOperation = function (trigOperation) {
 
-    if(this.answer === ''){
-         this.trigOperation = trigOperation;
-         if(this.operation===null && this.expression !=='' ){
-             this.operation = 'x';
-             return;
-         }
-         return;
+    if (this.answer === '') {
+        this.trigOperation = trigOperation;
+        if (this.operation === null && this.expression !== '') {
+            this.operation = 'x';
+            return;
         }
-        if(this.operation===null) this.operation = 'x';
-    this.trigOperation= trigOperation;
+        return;
+    }
+    if (this.operation === null) this.operation = 'x';
+    this.trigOperation = trigOperation;
     this.expression = this.answer;
     this.answer = '';
 }
 
 Calculator.prototype.compute = function () {
-    if(this.power){
+    if (this.power) {
         let computation;
-        const ans= this.answer;
+        const ans = this.answer;
         const exp = this.expression;
-        switch(this.power){
+        switch (this.power) {
             case 'x<sup>n</sup>':
                 computation = Math.pow(exp, ans);
                 break;
@@ -98,45 +98,45 @@ Calculator.prototype.compute = function () {
         }
         this.answer = computation;
         this.power = null;
-        this.expression='';
+        this.expression = '';
         console.log(this.answer);
         return;
     }
-    if(this.trigOperation!== null){
-        
+    if (this.trigOperation !== null) {
+
         const ans = parseFloat(this.answer);
         console.log(ans);
-        switch(this.trigOperation){
+        switch (this.trigOperation) {
             case 'Sin':
-            this.answer = Math.sin((ans/180)*Math.PI);
-            this.trigOperation = null;
-            this.compute();
-            break;
+                this.answer = Math.sin((ans / 180) * Math.PI);
+                this.trigOperation = null;
+                this.compute();
+                break;
             case 'Cos':
-            this.answer = ans===90? 0: Math.cos(Math.PI*(ans/180));
-            this.trigOperation = null;
-            this.compute();
-            break;
+                this.answer = ans === 90 ? 0 : Math.cos(Math.PI * (ans / 180));
+                this.trigOperation = null;
+                this.compute();
+                break;
             case 'Tan':
-                this.answer = ans===90? Infinity: Math.tan(Math.PI*(ans/180));
+                this.answer = ans === 90 ? Infinity : Math.tan(Math.PI * (ans / 180));
                 this.trigOperation = null;
                 this.compute();
-            break;
+                break;
             case 'ArcSin':
-                this.answer = Math.asin(ans)*180/Math.PI;
+                this.answer = Math.asin(ans) * 180 / Math.PI;
                 this.trigOperation = null;
                 this.compute();
-            break;
+                break;
             case 'ArcCos':
-                this.answer = Math.acos(ans)*180/Math.PI;
-                this.trigOperation = null;
-                this.compute();           
-             break;
-            case 'ArcTan':
-                this.answer = Math.atan(ans)*180/Math.PI;
+                this.answer = Math.acos(ans) * 180 / Math.PI;
                 this.trigOperation = null;
                 this.compute();
-                default:
+                break;
+            case 'ArcTan':
+                this.answer = Math.atan(ans) * 180 / Math.PI;
+                this.trigOperation = null;
+                this.compute();
+            default:
                 return;
         }
     }
@@ -144,8 +144,8 @@ Calculator.prototype.compute = function () {
     const exp = parseFloat(this.expression);
     const ans = parseFloat(this.answer);
     console.log(exp, ans)
-    if(isNaN(exp)||isNaN(ans)) return;  
-    switch (this.operation){
+    if (isNaN(exp) || isNaN(ans)) return;
+    switch (this.operation) {
         case '+':
             computation = exp + ans;
             break;
@@ -158,13 +158,13 @@ Calculator.prototype.compute = function () {
         case '÷':
             computation = exp / ans;
             break;
-            default:
-                console.log('bitch')
-                break;
+        default:
+            console.log('bitch')
+            break;
     }
     this.answer = computation;
     this.operation = null;
-    this.expression ='';
+    this.expression = '';
 }
 
 Calculator.prototype.getDisplayNumber = function (number) {
@@ -175,34 +175,36 @@ Calculator.prototype.getDisplayNumber = function (number) {
 
     let integerDisplay;
     if (isNaN(integerDigits)) {
-      integerDisplay = '';
+        integerDisplay = '';
     } else {
-      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+        integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0
+        });
     }
     if (decimalDigits != null) {
-      return `${integerDisplay}.${decimalDigits}`;
+        return `${integerDisplay}.${decimalDigits}`;
     } else {
-      return integerDisplay;
+        return integerDisplay;
     }
 }
 
 
 Calculator.prototype.updateDisplay = function () {
     this.displayAnswer.innerText = this.getDisplayNumber(this.answer);
-    if(this.operation != null ){
-        if(this.trigOperation!==null){
-        this.displayExpression.innerHTML = `${this.getDisplayNumber(this.expression)} ${this.operation}${this.trigOperation}`;
+    if (this.operation != null) {
+        if (this.trigOperation !== null) {
+            this.displayExpression.innerHTML = `${this.getDisplayNumber(this.expression)} ${this.operation}${this.trigOperation}`;
+        } else {
+            this.displayExpression.innerHTML = `${this.getDisplayNumber(this.expression)} ${this.operation}`
+        }
     } else {
-        this.displayExpression.innerHTML = `${this.getDisplayNumber(this.expression)} ${this.operation}`
-    }
-    } else {
-        if(this.trigOperation!==null){
+        if (this.trigOperation !== null) {
             this.displayExpression.innerText = this.trigOperation;
             return;
-     
+
         }
-        if(this.power!==null){
-            this.displayExpression.innerHTML= `${this.getDisplayNumber(this.expression)} ${this.power}`;
+        if (this.power !== null) {
+            this.displayExpression.innerHTML = `${this.getDisplayNumber(this.expression)} ${this.power}`;
             return;
         }
         this.displayExpression.innerText = '';
@@ -216,15 +218,15 @@ var $ = document.querySelector;
 var _ = document.querySelectorAll;
 
 
-let numbers = _.call(document,'[data-number]');
-let operations = _.call(document,'[data-operation]');
-let trigOperations = _.call(document,'[data-trig-operation]');
-let powers = _.call(document,'[data-power]');
-let clearAll = $.call(document,'[data-clear-all]');
-let prevdel = $.call(document,'[data-delete]');        //delete is a reserved keyword
-let equals = $.call(document,'[data-equals]');
-let displayExpression = $.call(document,'[data-expression]');
-let displayAnswer = $.call(document,'[data-answer]');
+let numbers = _.call(document, '[data-number]');
+let operations = _.call(document, '[data-operation]');
+let trigOperations = _.call(document, '[data-trig-operation]');
+let powers = _.call(document, '[data-power]');
+let clearAll = $.call(document, '[data-clear-all]');
+let prevdel = $.call(document, '[data-delete]'); //delete is a reserved keyword
+let equals = $.call(document, '[data-equals]');
+let displayExpression = $.call(document, '[data-expression]');
+let displayAnswer = $.call(document, '[data-answer]');
 
 
 
@@ -237,40 +239,38 @@ numbers.forEach(number => {
     })
 })
 
-operations.forEach(operation =>{
-    operation.addEventListener('click', () =>{
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
         calculator.selectOperation(operation.innerText);
         calculator.updateDisplay();
     })
 })
 
-trigOperations.forEach(operation =>{
+trigOperations.forEach(operation => {
     operation.addEventListener('click', () => {
         calculator.selectTrigOperation(operation.innerText);
         calculator.updateDisplay();
     })
 })
 
-powers.forEach(operation =>{
-    operation.addEventListener('click', () =>{
+powers.forEach(operation => {
+    operation.addEventListener('click', () => {
         calculator.selectPower(operation.innerHTML);
         calculator.updateDisplay();
     })
 })
 
-equals.addEventListener('click', (button) =>{
+equals.addEventListener('click', (button) => {
     calculator.compute();
     calculator.updateDisplay();
 })
 
-clearAll.addEventListener('click',(button)=>{
+clearAll.addEventListener('click', (button) => {
     calculator.clearAll();
     calculator.updateDisplay();
 })
 
-prevdel.addEventListener('click', (button)=>{
+prevdel.addEventListener('click', (button) => {
     calculator.delete();
     calculator.updateDisplay();
 })
-
-
